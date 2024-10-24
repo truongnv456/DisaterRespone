2f38977ac0b506df744738e68ca625928b333130
nguyenviettruong <truongnv2k1020@gmail.com>
2024-10-25 06:58:37 +0700

    Fix issues #GET_PASSES_THIS_REPO_UDACITY_PLEASE

--- train_classifier.py
+++ train_classifier.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import nltk
+from nltk.stem import WordNetLemmatizer
 from sqlalchemy import create_engine
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.pipeline import Pipeline, FeatureUnion
@@ -22,7 +23,7 @@ def self_tokenize(text):
     text = text.lower()
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     words = nltk.word_tokenize(text)
-    lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()
+    lemmatizer = WordNetLemmatizer()
     stop_words = set(nltk.corpus.stopwords.words('english'))
     lemmas = [lemmatizer.lemmatize(word, pos='v') for word in words if word not in stop_words]
     return lemmas
@@ -36,7 +37,7 @@ pipeline = Pipeline([
     ('features', FeatureUnion([
         ('tfidf', TfidfVectorizer()),
         ('count', CountVectorizer())
-    ])),
+    ])), # type: ignore
     ('clf', MultiOutputClassifier(RandomForestClassifier()))
 ])
 
@@ -47,13 +48,16 @@ y_pred = pipeline.predict(X_test)
 num_labels = y_test.values.shape[1]
 
 for i in range(num_labels):
-    report = classification_report(y_test.values[:, i], y_pred[:, i], output_dict=True)
-    for label, metrics in report.items():
-        if isinstance(metrics, dict):
-            print(f"Label: {label}")
-            print(f"Precision: {metrics['precision']:.4f}")
-            print(f"Recall: {metrics['recall']:.4f}")
-            print(f"F1-Score: {metrics['f1-score']:.4f}")
+    report = classification_report(y_test.values[:, i], y_pred[:, i], output_dict=True) # type: ignore
+    if isinstance(report, dict):
+        for label, metrics in report.items():
+            if isinstance(metrics, dict):
+                print(f"Label: {label}")
+                print(f"Precision: {metrics['precision']:.4f}")
+                print(f"Recall: {metrics['recall']:.4f}")
+                print(f"F1-Score: {metrics['f1-score']:.4f}")
+    else:
+        print("Error: classification_report did not return a dictionary.")
 
 with open('pipeline_new.pkl', 'wb') as file:
     pickle.dump(pipeline, file)
